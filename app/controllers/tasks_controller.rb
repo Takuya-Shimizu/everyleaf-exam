ffb07c113ab8b1b187bc17a162b6550a0fee2cc0
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-09 20:44:02 +0900

    タスクの一覧・詳細・新規作成・編集画面を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to new_task_path, notice: "タスクを登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
