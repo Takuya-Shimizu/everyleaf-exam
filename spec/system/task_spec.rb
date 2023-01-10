5fd2534384af17181cea3bcee0bc75fd731baf65
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-10 13:01:51 +0900

    テストコード実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,25 +4,28 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        task = FactoryBot.create(:task)
+        fill_in 'Title', with: 'test'
+        fill_in 'Content', with: 'test'
         click_button 'Create Task'
-        expect(page).to have_content 'test_content'
+        expect(page).to have_content 'test'
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
+        task = FactoryBot.create(:task, title: 'task1')
         visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'task1'
       end
     end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-
+        task = FactoryBot.create(:task)
+        visit task_path(task)
+        expect(page).to have_content 'test'
        end
      end
   end
