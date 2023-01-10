c12aa0597b0e7579179d15515449856ac220f091
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-10 23:26:19 +0900

    I18n化・タイムゾーン設定・タスク一覧の作成日時降順表示及びそのテスト実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'Title', with: 'test'
-        fill_in 'Content', with: 'test'
-        click_button 'Create Task'
+        fill_in 'タイトル', with: 'test'
+        fill_in '内容', with: 'test'
+        click_button '登録する'
         expect(page).to have_content 'test'
       end
     end
@@ -19,14 +19,22 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task1'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task = FactoryBot.create(:task, title: 'task1')
+        visit tasks_path
+        task_desc_test = all('td').first
+        expect(task_desc_test).to have_content 'task1'
+      end
+    end
   end
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
         task = FactoryBot.create(:task)
         visit task_path(task)
         expect(page).to have_content 'test'
-       end
-     end
+      end
+    end
   end
 end
