1937a959494adb7fb738a2d7b9b354902b98f8fa
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-23 16:40:41 +0900

    HEROKUデプロイ後データベースエラー

--- db/migrate/20230111002212_add_deadline_to_tasks.rb
+++ db/migrate/20230111002212_add_deadline_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddDeadlineToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :deadline, :date, null: false, default: "Wed, 11 Jan 2023"
-    change_column_default :tasks, :deadline, nil
+    add_column :tasks, :deadline, :date, null: false
   end
 end
