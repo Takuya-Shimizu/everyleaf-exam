e580ba7668d95ad4f63a9fc2edd50e995af8cd2c
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-26 14:51:46 +0900

    step4要件機能実装_訂正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-# 1.times do |n|
-#   name = Faker::Games::Pokemon.name
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                )
-# end
+1.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               )
+end
 
 User.create!(name:  "管理者D",
              email: "ddd@gmail.com",
